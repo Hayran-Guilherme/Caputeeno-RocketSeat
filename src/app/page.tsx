397ed8a683f1d55f0b890d58282d4a2887bcf421
Hayran Guilherme <hayran.gh@gmail.com>
2023-08-03 17:50:44 -0300

    feat: initial products fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
+"use client"
+
 import styles from './page.module.css'
+import { FilterBar } from './../components/Filters/flter-bar';
+import { ProductList } from '@/components/Products/products-list';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() {
+  const client = new QueryClient()
+
   return (
-    <>
-    </>
+    <QueryClientProvider client={client}> 
+      <main className={styles.main}>
+        <FilterBar/>
+        <ProductList/>
+      </main>
+    </QueryClientProvider>
+
   )
 }
